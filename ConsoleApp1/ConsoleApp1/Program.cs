c94c87a401d60cbb324ac4e90f586fa0acf7a9e8
Игнатьев Кирилл, Д19-191-2 <115728610+Kirillklain@users.noreply.github.com>
2023-03-24 18:28:16 +0400

    add ConsoleApp1

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,8 @@
+﻿PowerCollections.Stack<int> stack = new();
+Console.WriteLine(stack.Capacity);
+stack.Push(1);
+stack.Push(2);
+stack.Push(3);
+Console.WriteLine(stack.Pop());
+Console.WriteLine(stack.Pop());
+Console.WriteLine(stack.Pop());
